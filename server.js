4c9eebdcae71e7ec2fa62053df2104be335d1d11
JhonLP <JhonLPdev@gmail.com>
2015-05-31 00:50:46 -0700

    modificado puerto de servidor

--- server.js
+++ server.js
@@ -70,5 +70,5 @@ server.io.route('hello?', function (req) {
 	});
 });
 
-var port_number = server.listen(process.env.PORT || 3000);
-app.listen(port_number);
+var port_number = process.env.PORT || 3000;
+server.listen(port_number);
