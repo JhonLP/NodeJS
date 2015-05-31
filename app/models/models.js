c9d21f27d1a2689975092212922925d5cd9eb679
JhonLP <JhonLPdev@gmail.com>
2015-05-31 00:54:16 -0700

    modificado puerto de servidor

--- app/models/models.js
+++ app/models/models.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://<test>:<test>@ds043012.mongolab.com:43012/test');
+mongoose.connect('mongodb://test:test@ds043012.mongolab.com:43012/test');
 
 
 
