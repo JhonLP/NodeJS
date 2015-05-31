2000a3c337fceee5ce7b699f2200ed50d24f2a78
JhonLP <JhonLPdev@gmail.com>
2015-05-31 00:25:14 -0700

    agregado puerto de entorno

--- server.js
+++ server.js
@@ -70,4 +70,6 @@ server.io.route('hello?', function (req) {
 	});
 });
 
-server.listen(3000);
+server.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
